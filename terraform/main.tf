912d05f9a0df595543c81f1c0a014a7fb8e6c9e3
Chebrolu Harika <hchebrol@redhat.com>
2022-08-17 11:58:52 +0530

    added providers.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,6 @@
-provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x.
-    # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
-}
- 
-terraform {
-  backend "azurerm" {}
-}
- 
-resource "azurerm_resource_group" "demorg" {
-  name     = "demo-rg"
-  location = "Central US"
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
 }
  
 resource "azurerm_storage_account" "demosa" {
@@ -21,3 +10,44 @@ resource "azurerm_storage_account" "demosa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+
+# resource "azurerm_container_registry" "acr" {
+#   name                = var.acr_name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   sku                 = "Standard"
+#   admin_enabled       = true
+# }
+
+# data "azurerm_container_registry" "acr" {
+#   name                = var.acr_name
+#   resource_group_name = var.rg_name
+# }
+
+# resource "azurerm_container_group" "aci" {
+#   name                = "demoaci"
+#   resource_group_name = var.rg_name
+#   location            = var.location
+#   ip_address_type     = "public"
+#   dns_name_label      = "demoaci"
+#   os_type             = "Linux"
+
+#   image_registry_credential {
+#     username = data.azurerm_container_registry.acr.admin_username
+#     password = data.azurerm_container_registry.acr.admin_password
+#     server   = data.azurerm_container_registry.acr.login_server
+#   }
+
+#   container {
+#     name   = "demo"
+#     image  = "${data.azurerm_container_registry.acr.login_server}/aci:${var.build_id}"
+#     cpu    = "0.5"
+#     memory = "1.5"
+
+#     ports {
+#       port     = 80
+#       protocol = "TCP"
+#     }
+#   }
+# }
