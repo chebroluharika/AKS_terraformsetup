c20250ba88d3bfb390155af2723931c19b33ce96
Chebrolu Harika <hchebrol@redhat.com>
2022-08-18 19:58:04 +0530

    updated pipeline script for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,14 @@ resource "azurerm_resource_group" "sindhurg" {
   location = "centralus"
 }
 
+resource "azurerm_storage_account" "sindhusa" {
+  name                     = "sindhusatftest"
+  resource_group_name      = azurerm_resource_group.sindhurg.name
+  location                 = azurerm_resource_group.sindhurg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
 resource "azurerm_container_registry" "acr" {
   name                = "sindhuaciacr"
   resource_group_name = azurerm_resource_group.sindhurg.name
